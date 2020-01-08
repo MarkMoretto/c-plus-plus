bb143b8988dcdf03f873956fd76ca55dd29c983f
Mark Moretto <otteromkram@gmail.com>
2020-01-08 08:55:33 -0500

    Modified script; Requires header file now.

--- utils/filesystem/walk-directory.cxx
+++ utils/filesystem/walk-directory.cxx
@@ -1,22 +1,27 @@
 /*
-Recursively walk a file directory and return files based on extension
+Recursively walk a file directory
+Write results to text file
 
-Compiles successfully on Windows with msys2 and -std=c++17 flag
->>> g++ -std=c++17 -Wall -Wextra -o <executable name> <file name>.c__
+Takes arguments:
+    folder path (must use forward slash or double backslash)
+    file extension (with period at beginning)
 
-Optional syntax:
->>> g++ -std=c++17 -Wall -Wextra -o <executable name> <file name>.c__ -lstdc++fs -static-libgcc -static-libstdc++
 
+Run in bash:
+./walkdir <folder path> <extension>
 */
-
 #include <filesystem>
 #include <iostream>
+// #include <fstream>
 #include <string>
+#include <vector>
+
+#include "walk-directory.hxx"
 
 namespace fs = std::filesystem;
 
 
-std::string createPath(std::string root, std::string basename) {
+std::string CreatePath(std::string root, std::string basename) {
     return root + "\\" + basename;
 }
 
@@ -31,6 +36,7 @@ int main(int argc, char *argv[]) {
                 "If no extension is provided, the current action is to look for text files.";
 
         std::cout << argv[0] << arg_text << std::endl;
+        return 1;
     
     } else {
 
@@ -38,25 +44,42 @@ int main(int argc, char *argv[]) {
         std::string file_extension;
         std::string arg1;
         std::string arg2;
+        std::string output_path;
+
+        std::vector<std::string> results_vec;
 
         top_level_folder = argv[1];
+
+
         if (argv[2]) {
             file_extension = argv[2];    
         } else {
             file_extension = ".txt";    
         }
 
-        // std::string test_path = R"(C:\path\to\folder)";
-        // std::string file_extension = ".sql";
-        // std::string file_extension = ".txt";
-
+        
         fs::path current_dir(top_level_folder);
 
-
+        // Populate vector
         for (auto &file : fs::recursive_directory_iterator(current_dir)) {
             if (file.path().extension() == file_extension) {
-                std::cout << createPath(file.path().parent_path().string(), file.path().filename().string()) << std::endl;
+                results_vec.push_back(CreatePath(file.path().parent_path().string(), file.path().filename().string()));
+                // std::cout << createPath(file.path().parent_path().string(), file.path().filename().string()) << std::endl;
             }
         }
+
+        // Print results to console
+        for (const auto &element : results_vec) {
+            std::cout << element << std::endl;
+        }
+
+
+        // Write output to programming/Cpp/file-system directory
+        // This function comes from `utils.hxx`
+        output_path = "S:\\OFP\\Analytics\\Population Management\\Mark M\\Programming\\Cpp\\file-system\\walkdir-test.txt";
+        WriteResults(output_path, results_vec);
+
+
+        return 0;
     }
 }
