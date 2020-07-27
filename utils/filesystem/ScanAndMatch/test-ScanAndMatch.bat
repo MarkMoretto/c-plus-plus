53b754ffa37d2eeaf9b0fdbf542cbefc0ba69f88
Mark Moretto <otteromkram@gmail.com>
2020-07-26 19:56:03 -0500

    Update test-ScanAndMatch.bat

--- utils/filesystem/ScanAndMatch/test-ScanAndMatch.bat
+++ utils/filesystem/ScanAndMatch/test-ScanAndMatch.bat
@@ -6,12 +6,12 @@ SET PROMPT=
 SETLOCAL EnableDelayedExpansion
 SET MINGW64_HOME=
 :: Need to update with local msys install if compiling or running
-SET "msys64_pth=C:\Users\MMorett1\AppData\Local\Programs\msys64\mingw32"
+SET "msys64_pth=C:\Users\path\to\msys64\mingw32"
 SET "PATH=%msys64_pth%;%msys64_pth%\bin;%PATH%"
 
 
-SET "test_dir=S:\\OFP\\Analytics\\Population Management\\Queries\\Matt"
-SET keyword=Admission
+SET "test_dir=S:\\path\\to\\directory\\"
+SET keyword=MyKeyword
 SET file_extension=sql
 
 ECHO Running FileScanner test. Searching for %keyword% in %test_dir%...
